5e0b71c44e448dd23ed831d476bd765077a00085
Miluska Pajuelo <miluskapajuelo@gmail.com>
2021-09-24 18:23:12 -0500

    feat(component): create menu component and add image to login

--- src/components/Course/Card.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const CardCourse = () => (
-  <div>
-    <p>holi</p>
-  </div>
-);
-
-export default CardCourse;
---
+++ src/components/Menu/Menu.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Menu.scss';
+import CardCourse from '../CardCourse/CardCourse';
+
+const Menu = () => (
+
+  <section className="Menu">
+    <article className="Menu__image">
+      <p>holi menu</p>
+      <CardCourse />
+    </article>
+  </section>
+);
+
+export default Menu;
