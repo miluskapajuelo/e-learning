8798a396d89155d69ac133edd58971239fd60e66
Miluska Pajuelo <miluskapajuelo@gmail.com>
2021-09-26 00:31:52 -0500

    refactor(component): add and change functions, variables or styles

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Login from './components/Login/Login';
 import Menu from './components/Menu/Menu';
+import NewCourse from './components/NewCourse/NewCourse';
+import './global.scss';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" component={Menu} />
-        <Route path="/login" component={Login} />
+        <Route path="/menu" component={Menu} />
+        <Route path="/login" exact component={Login} />
+        <Route path="/create" component={NewCourse} />
       </Switch>
     </BrowserRouter>
   );
