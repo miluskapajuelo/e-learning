4b313562c4d8773bf0fefc41472695a8d26dba23
Miluska Pajuelo <miluskapajuelo@gmail.com>
2021-09-27 18:57:11 -0500

    service(database): save and get data in real time

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,23 +1,10 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/storage';
-// Initialize firebase services
-// Import the functions you need from the SDKs you need
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyAOk-hqj6FXKtjf912IsevXTbvp_rWR9i0',
-  authDomain: 'e-learning-konecta.firebaseapp.com',
-  projectId: 'e-learning-konecta',
-  storageBucket: 'e-learning-konecta.appspot.com',
-  messagingSenderId: '418345096115',
-  appId: '1:418345096115:web:414d43d4f006c3fab5b1c6',
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_FIREBASE_APPID,
 };
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-const fs = firebase.firestore();
-const storage = firebase.storage();
-export { fs, storage };
+export default firebaseConfig;
