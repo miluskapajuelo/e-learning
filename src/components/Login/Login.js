be4411a088583cbd3f49d7abe783556411889069
Miluska Pajuelo <miluskapajuelo@gmail.com>
2021-09-27 19:03:09 -0500

    feat(login): add authentication, alerts and integrate with services to login component

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,62 +1,124 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.scss';
 import {
-  FormControl, InputLabel, Input, Button, Grid, Paper, InputAdornment, Avatar,
+  TextField,
+  Button,
+  Grid,
+  Paper,
+  InputAdornment,
+  Avatar,
+  Container,
 } from '@material-ui/core';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import { AccountCircle } from '@material-ui/icons';
+import { useHistory } from 'react-router-dom';
 import login from '../../assets/images/login.svg';
+import getToken from '../../services/post';
+import { AlertSuccess, AlertError } from '../Alerts/Alert';
 
-const Login = () => (
+const Login = () => {
+  const [user, setUser] = useState('');
+  const [password, setPassword] = useState('');
+  const [openError, setOpenError] = useState(false);
+  const [openSuccess, setOpenSuccess] = useState(false);
 
-  <section className="login">
-    <article className="login__image">
-      <img src={login} alt="login" width="600px" />
-    </article>
-    <article className="login__container">
-      <Paper elevation={5} className="login__content" align="center">
-        <Grid className="mg-30">
-          <Avatar><LockOutlinedIcon /></Avatar>
-          <h1 className="mg-0">APRENDE</h1>
-          <h3>Iniciar Sesión</h3>
+  const history = useHistory();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const account = { user, password };
+    if (user !== '' || password !== '') {
+      const resp = await getToken(account);
+      if (resp.data.status === 200) {
+        const { token } = resp.data;
+        localStorage.setItem('token', token);
+        history.push('/menu');
+        setOpenSuccess(true);
+      } else {
+        setOpenError(true);
+      }
+    } else {
+      setOpenError(true);
+    }
+  };
+
+  return (
+    <>
+      <Grid container spacing={4} className="login">
+        <AlertError openError={openError} setOpenError={setOpenError} />
+        <AlertSuccess openSuccess={openSuccess} setOpenSuccess={setOpenSuccess} />
+        <Grid item xs={1} md={7} className="login__image">
+          <img src={login} alt="login" width="600px" />
         </Grid>
-        <FormControl variant="standard" style={{ width: 270 }}>
-          <InputLabel htmlFor="username">
-            Usuario
-          </InputLabel>
-          <Input
-            id="username"
-            placeholder="Introduce tu usuario"
-            startAdornment={(
-              <InputAdornment position="start">
-                <AccountCircle />
-              </InputAdornment>
-          )}
-          />
-        </FormControl>
-        <Grid className="mg-24">
-          <FormControl style={{ width: 270 }}>
-            <InputLabel htmlFor="password">
-              Contraseña
-            </InputLabel>
-            <Input
-              id="password"
-              placeholder="Introduce tu contraseña"
-              fullwidth
-              startAdornment={(
-                <InputAdornment position="start">
-                  <AccountCircle />
-                </InputAdornment>
-          )}
-            />
-          </FormControl>
+        <Grid item xs={12} md={5}>
+          <Paper elevation={5} className="login__content" align="center">
+            <Grid className="mg-30">
+              <Avatar>
+                <LockOutlinedIcon />
+              </Avatar>
+              <h1 className="mg-0">APRENDE</h1>
+              <h3>Iniciar Sesión</h3>
+            </Grid>
+            <Container>
+              <Grid
+                className="login__form"
+                container
+                spacing={3}
+                component="form"
+                autoComplete="off"
+                noValidate
+              >
+                <Grid item xs={12} md={12}>
+                  <TextField
+                    required
+                    id="usuario"
+                    label="Usuario"
+                    placeholder="Usuario"
+                    className="w-100"
+                    onChange={(e) => setUser(e.target.value)}
+                    InputProps={{
+                      startAdornment: (
+                        <InputAdornment position="start">
+                          <AccountCircle />
+                        </InputAdornment>
+                      ),
+                    }}
+                    variant="standard"
+                  />
+                </Grid>
+                <Grid item xs={12} md={12}>
+                  <TextField
+                    required
+                    id="contraseña"
+                    type="password"
+                    label="Contraseña"
+                    placeholder="Contraseña"
+                    className="w-100"
+                    onChange={(e) => setPassword(e.target.value)}
+                    InputProps={{
+                      startAdornment: (
+                        <InputAdornment position="start">
+                          <AccountCircle />
+                        </InputAdornment>
+                      ),
+                    }}
+                    variant="standard"
+                  />
+                  <Button
+                    style={{ padding: 12, width: 270, marginTop: 70 }}
+                    variant="contained"
+                    onClick={handleSubmit}
+                  >
+                    Iniciar sesiòn
+                  </Button>
+                </Grid>
+              </Grid>
+            </Container>
+          </Paper>
         </Grid>
-        <Grid className="mg-58" style={{ width: 270 }}>
-          <Button style={{ padding: 12, width: 270 }} variant="contained">Iniciar sesiòn</Button>
-        </Grid>
-      </Paper>
-    </article>
-  </section>
-);
+      </Grid>
+    </>
+  );
+};
 
 export default Login;
