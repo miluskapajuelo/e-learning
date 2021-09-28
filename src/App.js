dc265101e73bb07b95660c27242a4df352952340
Miluska Pajuelo <miluskapajuelo@gmail.com>
2021-09-27 21:40:55 -0500

    feat(style): add theme from blue to pink

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import Login from './components/Login/Login';
 import Menu from './components/Menu/Menu';
 import NewCourse from './components/NewCourse/NewCourse';
+import ModalVideo from './components/ModalVideo/ModalVideo';
 import './global.scss';
+import { createTheme, ThemeProvider } from '@material-ui/core';
+
+const theme = createTheme({
+  palette:{
+    primary:{
+      main: '#ED06B2'
+    }
+  }
+})
+
 
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/menu" component={Menu} />
-        <Route path="/login" exact component={Login} />
-        <Route path="/create" component={NewCourse} />
-      </Switch>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Switch>
+          <Redirect from="/" exact to="/login" />
+          <Route path="/login" component={Login} />
+          <Route path="/menu" component={Menu} />
+          <Route path="/create" component={NewCourse} />
+          <Route path="/video" component={ModalVideo} />
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 }
 export default App;
