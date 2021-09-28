b58a79fa2ba6e9a820402f2870f24974a3c72e67
Miluska Pajuelo <miluskapajuelo@gmail.com>
2021-09-27 21:35:24 -0500

    feat(service): add firebase service in menu component

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,14 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Menu.scss';
+import { useHistory } from 'react-router-dom';
 import { Typography, Button } from '@material-ui/core';
 import { AddCircleOutline } from '@material-ui/icons';
-import { useHistory } from 'react-router-dom';
+import { onSnapshot } from 'firebase/firestore';
 import CardCourse from '../CardCourse/CardCourse';
+import ModalVideo from '../ModalVideo/ModalVideo';
 import Sidebar from '../Sidebar/Sidebar';
+import { queryCollection } from '../../firebase/functions';
 
 const Menu = () => {
   const [openSidebar, setOpenSidebar] = useState(false);
-
+  const [post, setPosts] = useState([]);
   const history = useHistory();
 
   const onClickHandler = () => {
@@ -19,8 +22,17 @@ const Menu = () => {
     history.push('/create');
   };
 
+  useEffect(() => {
+    onSnapshot(queryCollection(), (querySnapshot) => {
+      setPosts(
+        querySnapshot.docs.map((doc) => ({ id: doc.id, data: doc.data() })),
+      );
+    });
+  }, []);
+
   return (
     <section className="section">
+      <ModalVideo />
       <article>
         <div className="section__header">
           <div
@@ -29,28 +41,24 @@ const Menu = () => {
             tabIndex={0}
             onMouseDown={onClickHandler}
           >
-            <img
-              src="https://www.grupokonecta.com/wp-content/uploads/2016/07/logo-konecta-pie.svg"
-              alt="logo"
-            />
             <Typography gutterBottom variant="h4" component="div">
               Cursos
+              {openSidebar}
             </Typography>
             <Typography variant="body1" color="text.secondary">
               Cursos para dar tus primeros pasos
             </Typography>
           </div>
-          <Button variant="outlined" startIcon={<AddCircleOutline />} onClick={createCourse} style={{ height: 45, marginRight: 5 }}>
-            Crear curso
-          </Button>
+          <div className="section__menu--burger">
+            <Button variant="outlined" startIcon={<AddCircleOutline />} onClick={createCourse} style={{ height: 45, marginRight: 5 }}>
+              Crear curso
+            </Button>
+          </div>
         </div>
         <Sidebar openSidebar={openSidebar} setOpenSidebar={setOpenSidebar} />
       </article>
       <article className="section__container">
-        <CardCourse />
-        <CardCourse />
-        <CardCourse />
-        <CardCourse />
+        {post.map((course, index) => <CardCourse course={course} key={index} />)}
       </article>
     </section>
   );
